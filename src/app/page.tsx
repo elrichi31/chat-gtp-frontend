574fa7de3268b9a467cc0b95fb4bb154ebe1aff6
Nicolas <Nicolas>
2023-11-15 08:39:08 -0500

    Vercel config new 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ function Chatty() {
 
     setQuery("");
     console.log(process.env.NEXT_PUBLIC_REACT_APP_API_URL)
-    const response = await fetch("https://newback-six.vercel.app/", {
+    const response = await fetch("https://newback-six.vercel.app/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
